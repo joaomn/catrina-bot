83424c93c3f552847945c5c39c802041a88c65de
joaomn <97440058+joaomn@users.noreply.github.com>
2024-11-13 20:46:25 -0300

    Refac: alterações de dataset

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ class Chatbot:
         master.grid_rowconfigure(2, weight=0)
 
         ctk.set_appearance_mode("dark")  # "light" ou "dark"
-        ctk.set_default_color_theme("dark-blue")  # Temas disponíveis: "blue", "green", "dark-blue"
+        ctk.set_default_color_theme("green")  # Temas disponíveis: "blue", "green", "dark-blue"
 
         # Área de texto
         self.text_area = ctk.CTkTextbox(master, width=500, height=300, wrap="word")
@@ -26,12 +26,12 @@ class Chatbot:
         self.text_area.configure(state="disabled")  # Apenas leitura na área de texto
 
         # Campo de entrada
-        self.entry = ctk.CTkEntry(master, width=400, placeholder_text="Digite o nome do aluno...")
+        self.entry = ctk.CTkEntry(master, width=400, placeholder_text="Digite o nome de um antibiotico(em ingles)...")
         self.entry.grid(row=1, column=0, padx=20, pady=10, sticky="ew")
         self.entry.bind("<Return>", self.process_input)
 
         # Botão de envio
-        self.send_button = ctk.CTkButton(master, text="Enviar", fg_color="blue", command=self.process_input)
+        self.send_button = ctk.CTkButton(master, text="Enviar", fg_color="green", command=self.process_input)
         self.send_button.grid(row=2, column=0, padx=20, pady=10)
 
     def process_input(self, event=None):
@@ -54,7 +54,7 @@ class Chatbot:
     def get_response(self, user_input):
         # Consultar a tabela do Google Sheets
         try:
-            response = requests.get("https://docs.google.com/spreadsheets/d/e/2PACX-1vSeRkcpQPm5eGkRh94-9tOyKBJ-1we50lvqHqIo_tnP3ZrwgbXZVQITq2pk671MNsYyurqZLUgCcpK-/pubhtml")
+            response = requests.get("https://docs.google.com/spreadsheets/d/1kH6bNpIH1vrn2fkyZZQzESIfl1llZa9IYgWPEYv_XHE/edit?usp=sharing")
             if response.status_code == 200:
                 soup = BeautifulSoup(response.text, 'html.parser')
                 rows = soup.find_all('tr')
@@ -64,10 +64,10 @@ class Chatbot:
                     cells = row.find_all('td')
                     nome = cells[0].get_text().strip()
                     if nome.lower() == user_input.lower():
-                        media = cells[1].get_text().strip()
-                        conceito = cells[2].get_text().strip()
-                        return f"Aluno: {nome}, Média: {media}, Conceito: {conceito}"
-                return "Aluno não encontrado. Por favor, verifique o nome e tente novamente."
+                        familia = cells[1].get_text().strip()
+                        uso = cells[2].get_text().strip()
+                        return f"O Antibiotico: {nome}, Pertence a Familia dos: {familia}, E é usado para: {uso}"
+                return "Antibiotico  não encontrado. Por favor, verifique o nome e tente novamente."
             else:
                 return "Desculpe, não consegui acessar a tabela no momento."
         except requests.exceptions.RequestException as e:
